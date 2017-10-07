6e009e1cfc0d464a6d794fd449bab05c31c26b50
noize21 <lukas.seltmann@live.de>
2017-10-07 14:39:18 +0200

    added 'node_id' column in brackets table

--- database/migrations/2017_10_05_200837_create_brackets_table.php
+++ database/migrations/2017_10_05_200837_create_brackets_table.php
@@ -14,14 +14,14 @@ class CreateBracketsTable extends Migration
     public function up()
     {
         Schema::create('brackets', function (Blueprint $table) {
-            $table->integer('id')->unsigned();
+            $table->increments('id');
             $table->integer('tournament_id')->unsigned();        
             $table->integer('team_id')->unsigned()->nullable();
-            $table->integer('parent_id')->nullable();
-            $table->integer('left_child_id')->nullable();
-            $table->integer('right_child_id')->nullable();
+            $table->integer('parent_id')->nullable()->unsigned();
+            $table->integer('left_child_id')->nullable()->unsigned();
+            $table->integer('right_child_id')->nullable()->unsigned();
+            $table->integer('node_id');
             
-            $table->primary(['id', 'tournament_id']);
             $table->foreign('tournament_id')->references('id')->on('tournaments')->onDelete('cascade');
             $table->foreign('team_id')->references('id')->on('local_teams')->onDelete('cascade');
             
