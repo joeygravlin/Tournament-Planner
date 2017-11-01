066b6213353077464a7d546df0c464b552a0bf2f
vossso <sophie.voss@stud.hs-hannover.de>
2017-11-01 13:38:16 +0100

    modified routes

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -9,19 +9,19 @@ let routes = [
     {
         path: '/about',
         component: require('./views/About')
-    }
+    },
     {
       path: '/create',
       component: require('./views/CreateT')
-    }
+    },
     {
       path: '/register',
       component: require('./views/Register')
-    }
+    },
     {
       path: '/login',
       component: require('./views/Login')
-    }
+    },
     {
       path: '/sign',
       component: require('./views/SignIn')
