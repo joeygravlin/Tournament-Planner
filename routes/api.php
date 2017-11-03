6e1402c1c7c4a1e24d8e564f957e90fad367ed09
Joseph Gravlin <joeygravlin@gmail.com>
2017-11-03 02:31:58 -0400

    Move relevant REST routes from web to api.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,29 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+
+
+//user specific
+Route::get('user','UserController@currentUser');
+Route::get('user/tournaments','UserController@tournaments');
+
+//tournament specific
+Route::get('tournament/{id}/users', 'TournamentController@users');
+Route::get('tournament/{id}/teams', 'TournamentController@teams');
+Route::get('tournament/{id}/brackets', 'TournamentController@brackets');
+
+//team specific
+Route::get('team/{id}/users','TeamController@users');
+Route::get('team/{id}/guests', 'TeamController@guests');
+
+//for any table: get row via 'table/id'
+Route::get('user/{user}', 'UserController@show');
+Route::get('team/{team}', 'TeamController@show');
+Route::get('tournament/{tournament}', 'TournamentController@show');
+Route::get('bracket/{bracket}', 'BracketController@show');
+Route::get('guest/{guest}','GuestController@show');
