e967083f8b251281219b259b4a8cc9efbbd0a4f7
Joseph Gravlin <joeygravlin@gmail.com>
2017-12-12 13:37:17 -0500

    Move api routes from web.php to api.php router.
    web.php now routes all requests, aside from the '/api' prefix,
    to the same template, letting vuejs frontend router handle the rest.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,50 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::middleware('auth:api')->group(function () {
+    //user specific
+    Route::get('user','UserController@currentUser');
+    Route::get('user/tournaments','UserController@tournaments');
+
+    //tournament specific
+    Route::get('tournament/{id}/users', 'TournamentController@users');
+    Route::get('tournament/{id}/teams', 'TournamentController@teams');
+    Route::get('tournament/{id}/brackets', 'TournamentController@brackets');
+
+    Route::put('tournament/{tid}/adduser/{uid}','TournamentController@addUser');
+    Route::delete('tournament/{tid}/remove/{uid}','TournamentController@removeUser');
+    Route::post('tournament/create','TournamentController@create');
+    Route::delete('tournament/{id}','TournamentController@deleteTournament');
+
+    //Brackets
+    Route::post('tournament/{id}/start','BracketController@initTree');
+    Route::post('win/{id}','BracketController@win');
+    Route::post('bracket/{id}/score/{score}','BracketController@setScore');
+
+    //team specific
+    Route::get('team/{id}/users','TeamController@users');
+    Route::get('team/{id}/guests', 'TeamController@guests');
+    Route::put('team/{tid}/adduser/{uid}','TeamController@addUser');
+    Route::delete('team/{tid}/remove/{uid}','TeamController@removeUser');
+    Route::put('team/{tid}/addguest/{name}','TeamController@addGuest');
+    Route::post('team/create','TeamController@create');
+    Route::delete('team/{id}','TeamController@deleteTeam');
+
+    //guest specific
+    Route::delete('guest/{id}','GuestController@deleteGuest');
+
+    //for any table: get row via 'table/id'
+    Route::get('user/{user}', 'UserController@show');
+    Route::get('team/{team}', 'TeamController@show');
+    Route::get('tournament/{tournament}', 'TournamentController@show');
+    Route::get('bracket/{bracket}', 'BracketController@show');
+    Route::get('guest/{guest}','GuestController@show');
 });
+
--- routes/web.php
+++ routes/web.php
@@ -12,54 +12,23 @@
 */
 
 
-//Route::any('{all}', function () {
-    //return view('layouts.app');
-//})->where(['all' => '.*']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * Eh, we'll just leave this here for now... Remove once API Auth is complete.
+ */
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+/**
+ * Match all URI's, except for the '/api' prefix,
+ * and route them to the main layout template.
+ */
+
+Route::any('{all}', function () {
+    return view('layouts.app');
+})->where(['all' => '^((?!api).)*$']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-//user specific
-Route::get('user','UserController@currentUser');
-Route::get('user/tournaments','UserController@tournaments');
-
-
-//tournament specific
-Route::get('tournament/{id}/users', 'TournamentController@users');
-Route::get('tournament/{id}/teams', 'TournamentController@teams');
-Route::get('tournament/{id}/brackets', 'TournamentController@brackets');
-
-Route::put('tournament/{tid}/adduser/{uid}','TournamentController@addUser');
-Route::delete('tournament/{tid}/remove/{uid}','TournamentController@removeUser');
-Route::post('tournament/create','TournamentController@create');
-Route::delete('tournament/{id}','TournamentController@deleteTournament');
-
-//Brackets
-Route::post('tournament/{id}/start','BracketController@initTree');
-Route::post('win/{id}','BracketController@win');
-Route::post('bracket/{id}/score/{score}','BracketController@setScore');
-
-//team specific
-Route::get('team/{id}/users','TeamController@users');
-Route::get('team/{id}/guests', 'TeamController@guests');
-Route::put('team/{tid}/adduser/{uid}','TeamController@addUser');
-Route::delete('team/{tid}/remove/{uid}','TeamController@removeUser');
-Route::put('team/{tid}/addguest/{name}','TeamController@addGuest');
-Route::post('team/create','TeamController@create');
-Route::delete('team/{id}','TeamController@deleteTeam');
-
-//guest specific
-Route::delete('guest/{id}','GuestController@deleteGuest');
-
-//for any table: get row via 'table/id'
-Route::get('user/{user}', 'UserController@show');
-Route::get('team/{team}', 'TeamController@show');
-Route::get('tournament/{tournament}', 'TournamentController@show');
-Route::get('bracket/{bracket}', 'BracketController@show');
-Route::get('guest/{guest}','GuestController@show');
